da44cd2b2c3e0ec3f52bacbe8ab51312cde23428
AhmedMaree <60566202+AhmedMaree@users.noreply.github.com>
2023-04-20 22:53:31 +0200

    Delete .bat

--- .bat
+++
@@ -1,9 +0,0 @@
-@echo off
-javac AutosarContainerReorderer.java
-if %errorlevel% neq 0 (
-    echo Compilation failed
-    pause
-    exit /b %errorlevel%
-)
-java AutosarContainerReorderer %1
-pause
