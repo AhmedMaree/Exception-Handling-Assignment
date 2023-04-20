20f7e747a84acabba3617a84f3ed17759bca9267
AhmedMaree <60566202+AhmedMaree@users.noreply.github.com>
2023-04-20 23:09:42 +0200

    Update Exception Handling.bat

--- Exception Handling.bat
+++ Exception Handling.bat
@@ -1,1 +1,10 @@
+@echo off
+javac AutosarContainerReorderer.java
+if %errorlevel% neq 0 (
+    echo Compilation failed
+    pause
+    exit /b %errorlevel%
+)
+java AutosarContainerReorderer %1
+pause
 
